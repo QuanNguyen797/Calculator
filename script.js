58292b5c6a83cf43f1895acd14ae28f4d91ac1f4
QuanNguyen797 <hoangquan797@gmail.com>
2025-05-10 10:45:31 +0700

    Implemented calculator functions: add, minus, multiply, devide.

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 console.log("Hello, world!");
+
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function devide(a, b) {
+    return a / b;
+};
+
+
+function operate(operator, a, b) {
+    if (operator == "+") {
+        add(a, b);
+    };
+    if (operator == "-") {
+        subtract(a, b);
+    };
+    if (operator == "*") {
+        multiply(a, b);
+    };
+    if (operator == "/") {
+        devide(a, b);
+    };
+};
+
+
+
